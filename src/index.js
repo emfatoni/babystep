91ff0353b5b357035fd7523940d180efef9a3eee
Muhammad Fatoni <em.fatoni@gmail.com>
2019-10-12 14:51:35 +0700

    react: sum and card

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class BSPProjectSum extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<p className="text-dark">26 days • 3 of 8 tasks completed.</p>
+		);
+	}
+}
+
+class BSPProjectCard extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div className="card bg-warning mb-3" title="A Project">
+				<div className="card-body text-light">
+					<h5 className="card-title">A Project</h5>
+					<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					<BSPProjectSum />
+				</div>
+			</div>
+		);
+	}
+}
