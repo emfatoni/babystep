41ef48fb6c8065da71c5bfdb6fbf5fb8b798fcde
Muhammad Fatoni <em.fatoni@gmail.com>
2019-11-06 06:07:00 +0700

    Updates

---
+++ precache-manifest.e9465fba88d603301d0f6694fb8699f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575eb421c6cff80d096ddd226e621acb",
+    "url": "/babystep/index.html"
+  },
+  {
+    "revision": "e50cf92f6fc7a59a0cda",
+    "url": "/babystep/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "59f51b21b5e792694acf",
+    "url": "/babystep/static/js/2.bf4a704c.chunk.js"
+  },
+  {
+    "revision": "e50cf92f6fc7a59a0cda",
+    "url": "/babystep/static/js/main.e6f00d95.chunk.js"
+  },
+  {
+    "revision": "268cbdfa7d969ce5700d",
+    "url": "/babystep/static/js/runtime-main.a6cec4bf.js"
+  }
+]);
